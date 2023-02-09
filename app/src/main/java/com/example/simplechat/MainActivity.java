719623a87e1f23fb7792c5cde68fa9cd6feeb520
krupk <MaxKR1994>
2023-02-09 13:23:26 +0200

    added comments for each method

--- app/src/main/java/com/example/simplechat/MainActivity.java
+++ app/src/main/java/com/example/simplechat/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.simplechat.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference databaseReference;
     UserAdapter userAdapter;
 
+//    This method is called when the activity is first created. It initializes the layout
+//    by inflating the ActivityMainBinding class, sets the adapter for the recycler view
+//    and retrieves the list of users from the Firebase database and populates the adapter
+//    with the data.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
         databaseReference = FirebaseDatabase.getInstance().getReference("users");
 
         databaseReference.addValueEventListener(new ValueEventListener() {
+
+//            This method is called when the data in the Firebase database changes.
+//            It clears the user adapter, loops through the children of the DataSnapshot object,
+//            and adds each child to the adapter, as long as it is not the current user.
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 userAdapter.clear();
@@ -44,11 +56,40 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             }
-
+//            This method is called when an error occurs while trying to retrieve the data
+//            from the Firebase database. It is typically used to handle the error and log it
+//            for debugging purposes.
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
+
+    }
+
+//    This method is used to create the options menu for the activity.
+//    It inflates the menu resource file main_menu and returns true
+//    to indicate that the menu has been created successfully
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+//    This method is called when a menu item is selected.
+//    It checks the selected item's ID and if it is equal to R.id.logout,
+//    it signs out the current user and starts the AuthenticationActivity
+//    to allow the user to log in again. If the item is not a logout menu item,
+//    the method returns false.
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.logout){
+            FirebaseAuth.getInstance().signOut();
+            startActivity((new Intent(MainActivity.this,AuthenticationActivity.class)));
+            finish();
+            return true;
+        }
+        return false;
     }
 }
